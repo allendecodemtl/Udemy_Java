b1c2175c4c5ce719a8061446bf42dfe9e1929d58
allendecodemtl <allen.decodemtl@gmail.com>
2019-01-29 15:43:19 -0500

    Variables

--- 01_ws_setup_first_steps/HelloWorld/src/Hello.java
+++ 01_ws_setup_first_steps/HelloWorld/src/Hello.java
@@ -1,5 +1,14 @@
 public class Hello {
     public static void main(String[] args) {
+
         System.out.println("Hello");
+
+        int myFirstNumber = 5;
+        int mySecondNumber = 12;
+        int myThirdNumber= 6;
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        System.out.println(myTotal);
+
     }
 }
---
+++ 02_ws_var_datatypes_ops/src/com/company/e_02/FloatAndDouble.java
@@ -0,0 +1,16 @@
+package com.company.e_02;
+
+public class FloatAndDouble {
+
+    public static void main(String[] args) {
+        int myIntValue = 5;
+        float myFloatValue = 5f;
+        double myDoubleValue = 5d;
+
+        System.out.println("myIntValue: " + myIntValue);
+        System.out.println("myFloatValue: " + myFloatValue);
+        System.out.println("myDoubleValue: " + myDoubleValue);
+
+
+    }
+}
