b76036ee52a686b08e55f1b40d26aa9e75387eff
allendecodemtl <allen.decodemtl@gmail.com>
2019-01-29 20:46:54 -0500

    Methods

---
+++ 02_ws_var_datatypes_ops/src/com/company/e_03/CharBoolean.java
@@ -0,0 +1,23 @@
+package com.company.e_03;
+
+public class CharBoolean {
+
+    public static void main(String[] args) {
+        // width of 16 (2 bytes)
+        char myChar = '\u00A9';
+        System.out.println(myChar);
+
+        // booelan
+        boolean myBoolean = false;
+        boolean isMale = true;
+
+
+        // Challenge
+        char registeredSymbol = '\u00AE';
+        System.out.println(registeredSymbol);
+
+    }
+
+}
+
+
---
+++ Section_04/src/com/company/L_02/WhiteSpaceIndenting.java
@@ -0,0 +1,15 @@
+package com.company.L_02;
+
+public class WhiteSpaceIndenting {
+
+    public static void main(String[] args) {
+
+        int myVariable = 50;
+        myVariable++;
+        myVariable--;
+        System.out.println("This is " +
+                "a " +
+                "test");
+
+    }
+}
