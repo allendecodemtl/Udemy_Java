eabb248cf920aa66ef2a42b23a4de9dffba7bb5e
allendecodemtl <allen.decodemtl@gmail.com>
2019-02-14 21:25:27 -0500

    Finished Lambda

--- Section_16_LambdaExpressions/src/L_Challenge/Main.java
+++ Section_16_LambdaExpressions/src/L_Challenge/Main.java
@@ -1,7 +1,10 @@
 package L_Challenge;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.function.Function;
+import java.util.function.Supplier;
 
 public class Main {
     public static void main(String[] args) {
@@ -19,6 +22,49 @@ public class Main {
 
         String myString = "Let's split this up into an array";
         System.out.println(everySecondChar.apply(myString));
+
+        System.out.println(everySecondCharacter(everySecondChar,myString));
+
+        Supplier<String> iLoveJava = () -> "I love Java!";
+        String supplierResult = iLoveJava.get();
+        System.out.println(supplierResult);
+
+
+        System.out.println("======================");
+        List<String> topNames2015 = Arrays.asList(
+                "Amelia",
+                "Olivia",
+                "emily",
+                "Isla",
+                "Ava",
+                "oliver",
+                "Jack",
+                "Charlie",
+                "harry",
+                "Jacob"
+        );
+
+        // Other
+        List<String> firstUpperCaseList = new ArrayList<>();
+        topNames2015.forEach((name) -> {
+            firstUpperCaseList.add(name.substring(0,1).toUpperCase() + name.substring(1));
+        });
+        firstUpperCaseList.sort((String::compareTo));
+        firstUpperCaseList.forEach(System.out::println);
+
+        // Lambda
+        topNames2015.stream()
+                .map(s -> s.substring(0,1).toUpperCase().concat(s.substring(1)))
+                .sorted(String::compareTo)
+                .forEach(System.out::println);
+
+        System.out.println(
+                topNames2015.stream()
+                .filter(s -> s.substring(0,1).toUpperCase().equals("A"))
+                .count());
+
+
+
 
     }
 
